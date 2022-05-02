9c361fdba8e83b5c975872ae62d7c9b96281af31
Martin Huna <martinhuna85@gmail.com>
2022-05-02 02:08:46 +0100

    random testing of index.js commands

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # Git
 First repository
-
-## Subheader
-
-Test test
-
-## Index file
-
-I added text to Index body
-
---
+++ index.js
@@ -0,0 +1,7 @@
+let welcomeEl = document.getElementById("welcome-el")
+let name = "Martin Huna "
+let greeting = "Welcome back, "
+
+welcomeEl.innerText = greeting + name
+
+welcomeEl.innerText = greeting + name + "👋"
